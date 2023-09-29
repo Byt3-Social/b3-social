c6cb6a25cd81c189a7103560dd9807b8db9c3ae5
Leandro Pereira <leojgpereira@gmail.com>
2023-09-28 23:05:18 -0400

    Adiciona o serviço de ações sociais ao Docker Compose

--- init.sql
+++ init.sql
@@ -3,3 +3,5 @@ CREATE DATABASE IF NOT EXISTS ms_authentication;
 CREATE DATABASE IF NOT EXISTS ms_prospeccao;
 
 CREATE DATABASE IF NOT EXISTS ms_analise_documental;
+
+CREATE DATABASE IF NOT EXISTS ms_acoes_sociais;
