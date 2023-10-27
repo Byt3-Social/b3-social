1aad8ee7ec5aaf9180d7c9055428fd8c666ca79c
Leandro Pereira <leojgpereira@gmail.com>
2023-10-27 16:38:13 -0400

    Atualiza docker-compose.yml

--- init.sql
+++ init.sql
@@ -5,3 +5,5 @@ CREATE DATABASE IF NOT EXISTS ms_prospeccao;
 CREATE DATABASE IF NOT EXISTS ms_analise_documental;
 
 CREATE DATABASE IF NOT EXISTS ms_acoes_sociais;
+
+CREATE DATABASE IF NOT EXISTS ms_acompanhamento;
